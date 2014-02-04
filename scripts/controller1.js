815919424327c0ca81260724bc480b86740344d0
lnrael <guoalber1@gmail.com>
2014-02-04 18:12:35 -0500

    Grid now shows some change request data
    Yup.

--- scripts/controller1.js
+++ scripts/controller1.js
@@ -6,17 +6,11 @@ app.controller('listController',['$http', '$scope', function($http, $scope){
 	    $scope.crs = data.changerequests;
 	});
 
-	$scope.myData = [{name: "Moroni", age: 50},
-                 {name: "Tiancum", age: 43},
-                 {name: "Jacob", age: 27},
-                 {name: "Nephi", age: 29},
-                 {name: "Enos", age: 34}];
 	$scope.gridOptions = {
-                    data: 'myData',
-                    columnDefs: [{ field:"name", displayName: "Name"},
-                        { field:"age", displayName: "Age"}]
+                    data: 'crs',
+//                    columnDefs: [{ field:"name", displayName: "Name"},
+//                        { field:"age", displayName: "Age"}]
                         };
-                    
 	this.add = function add(cr){
 	    newcr = new Object();
 	    angular.copy(cr,newcr);
