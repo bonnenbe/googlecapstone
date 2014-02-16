4d1f16cf2e324a84c018a4dc8dc68e1e660847c3
Nick Vogt <vogtnich@msu.edu>
2014-02-16 18:37:50 -0500

    converts string times to date objects in update page

--- scripts/controller1.js
+++ scripts/controller1.js
@@ -149,7 +149,12 @@ app.controller('updateController', function($routeParams, $http, $scope, $locati
 	$scope.priorities = ['routine', 'sensitive'];
 	$http.get('/changerequests/' + $routeParams.id,"").success(function(data) {
 	    $scope.cr = data.changerequest;
+	    $scope.cr.startDate = new Date($scope.cr.startTime);
+	    $scope.cr.startTime = new Date($scope.cr.startTime);
+	    $scope.cr.endDate = new Date($scope.cr.endTime);
+	    $scope.cr.endTime = new Date($scope.cr.endTime);
 	});
+
 	this.update = function update(cr){
 	    $http.put('/changerequests/' + $routeParams.id,JSON.stringify(cr)).success(function(data) {
 		$location.path('#');
