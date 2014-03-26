eaaf0a20528054f2a9fdae879d4fa374a45bf7dc
lnrael <guoalber1@gmail.com>
2014-03-26 16:07:18 -0400

    removed extra alert and failed comments

--- controllers/controller-search.js
+++ controllers/controller-search.js
@@ -1,7 +1,6 @@
 app.controller('searchController', ['$http', '$scope', '$location', '$interval', '$routeParams',
     function ($http, $scope, $location, $interval, $routeParams) {
-        
-        
+
         // Note: initialization of sortfield is here and in search.html
         $scope.sortField = "created_on";
         $scope.direction = "descending";
@@ -32,73 +31,41 @@ app.controller('searchController', ['$http', '$scope', '$location', '$interval',
             'status'
         ];
 
-        $scope.searchParams = [
-            {
-                field: "summary",
-                text: ""
-            }, {
-                field: "technician",
-                text: ""
-            }, {
-                field: "priority",
-                text: "routine"
-            }, {
-                field: "tags",
-                text: ""
-            }, {
-                field: "startTime",
-                text: ""
-            }
-        ];
-        
-        
+        $scope.searchParams = [{
+            field: "summary",
+            text: ""
+        }, {
+            field: "technician",
+            text: ""
+        }, {
+            field: "priority",
+            text: "routine"
+        }, {
+            field: "tags",
+            text: ""
+        }, {
+            field: "startTime",
+            text: ""
+        }];
+
+
         this.buildQuery = function buildQuery() {
             $scope.query = ""
             $scope.searchParams.forEach(function (s) {
-                if (s.field != "" && s.text) { 
+                if (s.field != "" && s.text) {
                     $scope.query += s.field + ":" + s.text;
                     $scope.query += " ";
                 }
-                
             });
-
         }
-        
+
         this.search = function search() {
-            
-            this.buildQuery();            
-            alert($scope.sortField);
+
+            this.buildQuery();
             $location.search('query', $scope.query);
             $location.search('sort', $scope.sortField);
             $location.search('direction', $scope.direction);
             $location.path("/");
-            
-            //$location.search({query:$scope.query});
-            //$location.path("#");
-            
-            // var params = {};
-            // params["offset"] = 0;
-
-            // // Send full text query
-            // if (!query)
-                // query = "asdf";
-            // params["query"] = encodeURIComponent(query);
-
-            // params["limit"] = 10;
-            // //searchParams.forEach(function (s) {
-            // //    if (s.text) params[s.field] = s.text;
-            // //});
-            // var obj = {};
-
-            // obj["params"] = params;
-        
-            // obj["params"]["query"] = encodeURIComponent(query);
-            // $http.get('/changerequests', obj).success(function (data) {
-                // alert("not linked yet");
-            // });
-            
         }
-        
-        
     }
 ]);
